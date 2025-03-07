5d62982d0e452461699a4c2b19b942e391b83011
mpagni <Marco.Pagni@sib.swiss>
2025-03-07 08:54:43 +0100

    simplify error reporting

--- src/kgsteward/common.py
+++ src/kgsteward/common.py
@@ -76,10 +76,10 @@ def http_call( request_args, status_code = [ 200 ], echo = True ):
     r = requests.request( **request_args )
     end_time = time.time()
     if r.status_code not in status_code :
-        # dumper.dump( request_args )
+        dumper.dump( request_args )
         print_warn( "Status code = " + str( r.status_code ))
         print_warn( r.text if not r.text is None else '' )
-        raise RuntimeError( "HTTP request failed!" )
+        stop_error( "HTTP request failed!" )
     report( "elapsed time", ( "%.3f" % ( end_time - start_time )))
     return r
 
